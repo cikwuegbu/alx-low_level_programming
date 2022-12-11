d54a638b9c0e466201a20caf173bae3c6a01410e
cikwuegbu <eucheria.ikwuegbu@gmail.com>
2022-12-11 12:15:06 -0800

    assigns a random number to n and prints a string

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int lastNumber;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastNumber = n % 10;
+if (lastNumber > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastNumber);
+}
+else if (lastNumber == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastNumber);
+}
+else if (lastNumber < 6 && lastNumber != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNumber);
+}
+return (0);
+}

35f484cb693c9542d6561403f9767afa520d76f8
cikwuegbu <eucheria.ikwuegbu@gmail.com>
2022-12-14 20:09:38 -0800

    ;;

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 #include <stdio.h>
-#endif
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0 (Success)
  */
-int _putchar(char c);
 void print_alphabet(void)
 {
 char c = 'a';
 while (c <= 'z')
 {
 _putchar(c);
-c++
+c++;
 }
 _putchar('\n');
-return (0);
 }

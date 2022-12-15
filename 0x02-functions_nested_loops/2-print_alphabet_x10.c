4c57242a0961cbfebf666a4e10e18b0f993276dd
cikwuegbu <eucheria.ikwuegbu@gmail.com>
2022-12-14 21:02:06 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,19 @@
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
-int i = 0
+{
+int i;
 char c;
+i = 0;
 while (i < 10)
 {
 for (c = 'a'; c <= 'z'; c++)
+}
 {
 _putchar (c);
 }
 {
 _putchar('\n');
-i++
+i++;
 }
 }

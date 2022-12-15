042647f1c0f845c2dbb7557db34a694001516b99
cikwuegbu <eucheria.ikwuegbu@gmail.com>
2022-12-14 19:59:28 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#endif
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c);
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++
+}
+_putchar('\n');
+return (0);
+}
